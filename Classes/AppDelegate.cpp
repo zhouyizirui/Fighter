bd826caf800e6a35405a3cfa5e5ddd0f3ec3c077
zhouyizirui <zhouyizirui@gmail.com>
2014-09-19 21:31:24 +0800

    Fix memory bugs

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -26,6 +26,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     pDirector->setAnimationInterval(1.0 / 60);
 
     
+    
     // create a scene. it's an autorelease object
     CCScene *pScene = GameBoardController::scene();
     
